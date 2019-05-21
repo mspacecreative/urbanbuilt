5143abfba2de7ce4058ce4ec91e63252d64ca0d4
Matt <matt@mspacecreative.com>
2019-05-21 15:25:46 -0300

    c

--- wp-content/themes/urbanbuilt/js/scripts.js
+++ wp-content/themes/urbanbuilt/js/scripts.js
@@ -1,28 +1,32 @@
-function offsetSpacerHeight() {
-	$('.offset-spacer').each(function(index, obj) {
-		$(this).height($(this).parent().parent().parent().prev('.et_pb_column').height()/2);
+jQuery(document).ready(function($) {
+
+	function offsetSpacerHeight() {
+		$('.offset-spacer').each(function(index, obj) {
+			$(this).height($(this).parent().parent().parent().prev('.et_pb_column').height()/2);
+		});
+	}
+	
+	function offsetRowHeight() {
+		$('.offset-blocks-container .et_pb_row').each(function (index, obj) {
+			$(this).height($(this).children('.total-column-height').height());
+		});
+	}
+	
+	/*
+	$(document).ready(function () {
+		offsetSpacerHeight();
+		offsetRowHeight();
 	});
-}
-
-function offsetRowHeight() {
-	$('.offset-blocks-container .et_pb_row').each(function (index, obj) {
-		$(this).height($(this).children('.total-column-height').height());
+	*/
+	
+	$(window).load(function () {
+		offsetSpacerHeight();
+		offsetRowHeight();
 	});
-}
+	
+	$(window).resize(function () {
+		offsetSpacerHeight();
+		offsetRowHeight();
+	});
 
-/*
-$(document).ready(function () {
-	offsetSpacerHeight();
-	offsetRowHeight();
-});
-*/
-
-$(window).load(function () {
-	offsetSpacerHeight();
-	offsetRowHeight();
-});
-
-$(window).resize(function () {
-	offsetSpacerHeight();
-	offsetRowHeight();
 });
